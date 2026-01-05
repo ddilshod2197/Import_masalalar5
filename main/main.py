6f5c31dd46c26cc0817da5323cc0fe024afab0e6
ddilshod2197 <ddilshod2197@gmail.com>
2026-01-05 12:38:42 +0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,12 @@
+import talaba
+
+baholar = [85, 90, 78, 92, 88]
+
+res1 = talaba.ortacha_baho(baholar)
+print(res1)
+
+res2 = talaba.eng_yuqori(baholar)
+print(res2)
+
+res3 = talaba.eng_past(baholar)
+print(res3)
